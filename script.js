b10361152d2d70e05f5dc3cab6f9bcb67f439253
BarrettDamien <125761024+BarrettDamien@users.noreply.github.com>
2023-03-27 19:30:43 +0100

    Week 9 Lab
    Updating files for site with assignments for week 9. Using navbar to host both the bottle of beer lyrics and game in single host.

--- script.js
+++ script.js
@@ -1,48 +1,98 @@
 var computerChoice = ["rock","paper","scissors"];
-var randNum = Math.floor(Math.random()*3);
-var computer = computerChoice[randNum];
+
 var result = document.getElementById("message");
- 
+
+var movesLeft = 2;
 
 function playerRock(){
-	if(computer == "paper"){
-		result.textContent = "Computer Won";
-	}
-	else if(computer == "scissors"){
-		result.textContent = "Player Won";
-	}
-	else if(computer == "rock"){
-		result.textContent = "It's a TIE!";
+	var randNum = Math.floor(Math.random()*3);
+	var computer = computerChoice[randNum];
+	if(movesLeft>0){
+		if(computer == "paper"){
+			result.textContent = "Computer Won";
+			movesLeft=movesLeft-1;
+		}
+		else if(computer == "scissors"){
+			result.textContent = "Player Won";
+			movesLeft=movesLeft-1;
+
+		}
+		else if(computer == "rock"){
+			result.textContent = "It's a TIE!";
+			movesLeft=movesLeft-1;
+		}
+		else{
+			result.textContent = "---ERROR---";
+		}
 	}
 	else{
-		result.textContent = "---ERROR---";
+		document.getElementById("btn").style.visibility="hidden";
+		document.getElementById("refresh").style.display="visible";
+		document.getElementById("movesleft").innerHTML = "GAME OVER";
+		document.getElementById("refresh").innerHTML = "Please Refresh Page";
 	}
 }
 function playerPaper(){
-	if(computer == "scissors"){
-		result.textContent = "Computer Won";
-	}
-	else if(computer == "rock"){
-		result.textContent = "Player Won";
-	}
-	else if(computer == "paper"){
-		result.textContent = "It's a TIE!";
+	var randNum = Math.floor(Math.random()*3);
+	var computer = computerChoice[randNum];
+	if(movesLeft>0){
+		if(computer == "scissors"){
+			result.textContent = "Computer Won";
+			movesLeft=movesLeft-1;
+		}
+		else if(computer == "rock"){
+			result.textContent = "Player Won";
+			movesLeft=movesLeft-1;
+		}
+		else if(computer == "paper"){
+			result.textContent = "It's a TIE!";
+			movesLeft=movesLeft-1;
+		}
+		else{
+			result.textContent = "---ERROR---";
+		}
 	}
 	else{
-		result.textContent = "---ERROR---";
+		document.getElementById("btn").style.visibility="hidden";
+		document.getElementById("refresh").style.display="visible";
+		document.getElementById("movesleft").innerHTML = "GAME OVER";
+		document.getElementById("refresh").innerHTML = "Please Refresh Page";
 	}
 }
 function playerScissors(){
-	if(computer == "rock"){
-		result.textContent = "Computer Won";
-	}
-	else if(computer == "paper"){
-		result.textContent = "Player Won";
-	}
-	else if(computer == "scissors"){
-		result.textContent = "It's a TIE!";
+	var randNum = Math.floor(Math.random()*3);
+	var computer = computerChoice[randNum];
+	if(movesLeft>0){
+		if(computer == "rock"){
+			result.textContent = "Computer Won";
+			movesLeft=movesLeft-1;
+		}
+		else if(computer == "paper"){
+			result.textContent = "Player Won";
+			movesLeft=movesLeft-1;
+		}
+		else if(computer == "scissors"){
+			result.textContent = "It's a TIE!";
+			movesLeft=movesLeft-1;
+		}
+		else{
+			result.textContent = "---ERROR---";
+		}
 	}
 	else{
-		result.textContent = "---ERROR---";
+		document.getElementById("btn").style.visibility="hidden";
+		document.getElementById("refresh").style.display="visible";
+		document.getElementById("movesleft").innerHTML = "GAME OVER";
+		document.getElementById("refresh").innerHTML = "Please Refresh Page";
+	}
+}
+function song(){
+	for(i=99;i>=0;i=i-1){
+		if(i>0){
+			document.getElementById("lyrics").innerHTML+="</br>"+i+" bottles of beer on the wall, "+i+" bottles of beer.</br>Take one down and pass it around, "+(i-1)+" bottles of beer on the wall</br></br>";
+		}
+		else{
+			document.getElementById("lyrics").innerHTML+="</br>"+i+" bottles of beer on the wall, "+i+" bottles of beer.</br>No more to take down, that's the end of the round, time to climb down off of the wall";
+		}
 	}
 }
